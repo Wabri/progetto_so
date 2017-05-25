e2a2d488cbeae710e5708407482901ade0f338e0
pagano <pagano@debian>
2017-05-25 16:25:14 +0200

    not woking, trying to split the string in tokens for parsing

---
+++ progetto_so/commandexe/cmd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CMN_LEN 100
+
+char* parse(char cmd[]){
+
+char * pch;
+  printf ("Splitting string \"%s\" into tokens:\n",cmd);
+  pch = strtok (cmd," ");
+  while (pch != NULL)
+  {
+    printf ("%s\n",pch);
+    pch = strtok (NULL, " ");
+  }
+  return 0;
+}
+
+
+int main()
+{
+    int k = 1;
+    char cmd[MAX_CMN_LEN] = "";
+    char* dhn;
+    
+    while(1)
+    {
+    	printf("Inserisci il %d comando: ", k);
+	scanf("%s",&cmd);
+        dhn = parse(cmd);
+        printf("ecco il dhn : %s",dhn);
+	if(cmd=="\n")
+	{
+	    printf("Error!");
+	}
+	
+	// system(cmd);
+
+	k = k+1;   
+
+    }
+    
+    return(0);
+    
+}    
+
+
