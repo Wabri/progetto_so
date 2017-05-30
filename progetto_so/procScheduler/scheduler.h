89405195fa3fdf3d59def081288326845ec75c1e
Wabri <gabriele.puliti@gmail.com>
2017-05-30 12:50:43 +0200

    init scheduling library

--- progetto_so/procScheduler/scheduler.h
+++ progetto_so/procScheduler/scheduler.h
@@ -4,4 +4,30 @@
  *  Created on: 25 mag 2017
  *      Author: wabri
  */
+#include <string.h>
 
+typedef struct TaskElement {
+	int ID;
+	char nameTask[8];
+	int priority; // number between 0 and 9
+	int remainingExe; // max 99 execution per task
+	struct Task *nextTask; // task successivo
+} Task;
+
+void newTaskElement(Task*, int, char[], int, int);
+
+int runScheduling() {
+	Task *firstTask = malloc(sizeof(Task));
+	Task *lastTask;
+	lastTask = firstTask;
+	newTaskElement(lastTask,10,"test",5,40);
+	printf("name of first Task: %s \n\r", lastTask->nameTask);
+}
+
+void newTaskElement(Task *actualTask, int idT, char nameT[], int priorityT, int execT) {
+	actualTask->ID = idT;
+	strcpy(actualTask->nameTask,nameT);
+	actualTask->priority = priorityT;
+	actualTask->remainingExe = execT;
+	actualTask->nextTask = malloc(sizeof(Task));
+}
