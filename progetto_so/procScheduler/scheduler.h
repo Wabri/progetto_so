2566b946378d50914c3eb6a2a4f3457745d43efd
Wabri <gabriele.puliti@gmail.com>
2017-05-30 17:02:51 +0200

    create switch choice, there is a problem with the print of procedure

--- progetto_so/procScheduler/scheduler.h
+++ progetto_so/procScheduler/scheduler.h
@@ -5,6 +5,7 @@
  *      Author: wabri
  */
 #include <string.h>
+#include <stdio.h>
 
 typedef struct TaskElement {
 	int ID;
@@ -15,28 +16,66 @@ typedef struct TaskElement {
 } Task;
 
 void newTaskElement(Task*, int, char[], int, int);
+int getChoice();
+void printTask(Task*);
 int createID(Task*);
 char* getName(Task*);
 int getPriority(Task*);
 int getRemainingExecution(Task*);
 
-
 int runScheduling() {
-	printf("               This is a Scheduler Process\n\r");
-	printf(".......................................................................\n\r");
+	int flag = 1;
 	Task *firstTask = malloc(sizeof(Task));
 	Task *actualTask;
 	actualTask = firstTask;
-	while (1) {
-		break;
+	printf("               This is a Scheduler Process\n\r");
+	printf(
+			".......................................................................\n\r");
+	while (flag == 1) {
+		switch (getChoice()) {
+		case 0:
+			printf("Bye!!!\n\r");
+			return 0;
+		case 1:
+			printTask(firstTask);
+			break;
+		case 2:
+			newTaskElement(actualTask, 10, "test", 4, 5);
+			break;
+		default:
+			flag = 0;
+			break;
+		}
 	}
 	return 0;
 }
 
-void newTaskElement(Task *actualTask, int idT, char nameT[], int priorityT,	int execT) {
+int getChoice() {
+	printf("You can chose to:\n\r");
+	printf(" 0) Exit\n\r 1) Print Tasks\n\r 2) Create new Task\n\r");
+	int res = 0;
+	printf("> ");
+	scanf("%i", &res);
+	printf("you choice %i \n\r", res);
+	return res;
+}
+
+void printTask(Task *first) {
+	printf("The list of task is: \n\r");
+	for (Task *i = (Task*)first; i == NULL; i =(Task*) i->nextTask) {
+		printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n");
+		printf("| %d + %d         + %s        + %d                | ", i->ID,
+				i->priority, i->nameTask, i->priority);
+		printf("+----+-----------+-----------+-------------------+ \n");
+	}
+}
+
+void newTaskElement(Task *actualTask, int idT, char nameT[], int priorityT,
+		int execT) {
+	actualTask = malloc(sizeof(Task));
 	actualTask->ID = idT;
 	strcpy(actualTask->nameTask, nameT);
 	actualTask->priority = priorityT;
 	actualTask->remainingExe = execT;
-	actualTask->nextTask = malloc(sizeof(Task));
+	actualTask->nextTask = NULL;
 }
