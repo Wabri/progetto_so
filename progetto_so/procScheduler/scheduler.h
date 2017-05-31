cece5121728fe56dcdbde7618361883f17ae82f4
Wabri <gabriele.puliti@gmail.com>
2017-05-31 18:24:25 +0200

    added switch policy, modify priority and execution, and other small fixes

--- progetto_so/procScheduler/scheduler.h
+++ progetto_so/procScheduler/scheduler.h
@@ -12,46 +12,50 @@ typedef struct TaskElement {
 	int ID;
 	char nameTask[8];
 	int priority;
-	int remaningExe;
+	int remainingExe;
 	struct TaskElement *nextTask;
 } Task;
 
 int getChoice();
-void printAllTasks(Task*, Task*);
+void printAllTasks(Task*, Task*, char);
 void printTask(Task*);
 int checkEmptyList(Task*);
 void deleteTask(Task*);
 int execTask(Task*);
 Task* selectTask(Task*);
-char* sepa = "+----+-----------+-----------+-------------------+ \n\r";
 int getExeNumber();
 int getPriority();
 void getTaskName(Task*);
 Task* newTaskElement(Task*, int);
-void executionTask(Task*, char);
+int executeTask(Task*, char);
 void deleteTask(Task*);
+void modifyExecNumb(Task*);
+void modifyPriority(Task*);
 char switchPolicy(char);
+
+char* sepa = "+----+-----------+-----------+-------------------+ \n\r";
 
 int runScheduling() {
 	int idTraker = 1;
 	int flag = 1;
 	char policy = 'p';
+	const char* header =
+			".......................................................................\n\r";
 	Task *firstTask = malloc(sizeof(Task));
 	Task *lastTask = NULL; // the last Task is always empty
 	Task *tmpTask;
-	printf(
-			".......................................................................\n\r");
+	printf("%s", header);
 	printf("               This is a process scheduler\n\r");
-	printf(
-			".......................................................................\n\r");
+	printf("%s", header);
 	while (flag == 1) {
 		switch (getChoice()) {
 		case 0:
 			printf("Bye!\n\r");
 			return 0;
 		case 1:
-			if (checkEmptyList(firstTask) == 1)
+			if (checkEmptyList(firstTask) == 1) {
 				break;
+			}
 			printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n\r");
 			printf("This task has the value of:\n\r");
 			printTask(firstTask);
@@ -60,46 +64,35 @@ int runScheduling() {
 			if (checkEmptyList(firstTask)) {
 				break;
 			}
-			printAllTasks(firstTask, lastTask);
 			break;
 		case 3:
 			if (firstTask->ID == 0) {
 				lastTask = newTaskElement(firstTask, idTraker);
-				printAllTasks(firstTask, lastTask);
 			} else {
-				tmpTask = lastTask;
 				lastTask = newTaskElement(lastTask, idTraker);
-				printAllTasks(firstTask, lastTask);
 			}
 			idTraker += 1;
 			break;
 		case 4:
-			executeTask(firstTask);
-			printAllTasks(firstTask,lastTask);
+			executeTask(firstTask, policy);
 			break;
 		case 5:
 			deleteTask(firstTask);
-			printAllTasks(firstTask,lastTask);
-      break
-    case 6:
-			printf("\n\rYou change the policy of scheduling from ");
-			if (policy == 'p') {
-				printf("priority ");
-			} else if (policy == 'e') {
-				printf("remaining execution ");
-			}
+			break;
+		case 6:
 			policy = switchPolicy(policy);
-			printf("to ");
-			if (policy == 'p') {
-				printf("priority \n\r");
-			} else if (policy == 'e') {
-				printf("remaining execution \n\r");
-			}
+			break;
+		case 7:
+			modifyPriority(firstTask);
+			break;
+		case 8:
+			modifyExecNumb(firstTask);
 			break;
 		default:
 			flag = 0;
 			break;
 		}
+		printAllTasks(firstTask,lastTask,policy);
 	}
 	return 0;
 }
@@ -107,24 +100,23 @@ int runScheduling() {
 Task* selectTask(Task* t) {
 	int id;
 	printf("Seleziona il task...\nInserisci l'ID : ");
-	scanf("%d",&id);
-	while(t->ID!=id) 
+	scanf("%d", &id);
+	while (t->ID != id)
 		t = t->nextTask;
 	return t;
 }
 
-void deleteTask(Task* actualTask){
+void deleteTask(Task* actualTask) {
 	actualTask = selectTask(actualTask);
 	actualTask = NULL;
 	return;
 }
 
-int executeTask(Task* actualTask) {
+int executeTask(Task* actualTask, char pol) {
 	actualTask = selectTask(actualTask);
-	actualTask->remaningExe = actualTask->remaningExe-1;
-	return actualTask->remaningExe;
+	actualTask->remainingExe = actualTask->remainingExe - 1;
+	return actualTask->remainingExe;
 }
-
 
 int checkEmptyList(Task *actualTask) {
 	if (!(actualTask->ID)) {
@@ -168,17 +160,25 @@ int getChoice() {
 	printf("\n\rYou can choose to:\n\r");
 	printf(" 0) Exit\n\r 1) Print a task\n\r 2) Print all tasks\n\r");
 	printf(" 3) Create a new task\n\r 4) Execute Task\n\r");
-	printf(" 5) Delete Task\n\r 6) Switch policy\n\r");
+	printf(" 5) Delete Task\n\r 6) Switch policy (default : PRIORITY)\n\r");
+	printf(" 7) Modify priority\n\r 8) Modify number of remaining execution");
 	int res = 0;
-	printf("> ");
+	printf("\n\r> ");
 	scanf("%i", &res);
 	return res;
 }
 
-void printAllTasks(Task *first, Task *last) {
+void printAllTasks(Task *first, Task *last, char pol) {
+	printf("\n\rScheduling Policy: ");
+	if (pol == 'p') {
+		printf("PRIORITY ");
+	} else if (pol == 'e') {
+		printf("REMAINING EXECUTIONS ");
+	}
 	printf("\nThe list of tasks is: \n\r");
-	printf(sepa);
+	printf("%s", sepa);
 	printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n");
+	printf("%s", sepa);
 	Task* tmp = first;
 	while (tmp != last) {
 		printTask(tmp);
@@ -187,31 +187,44 @@ void printAllTasks(Task *first, Task *last) {
 }
 
 void printTask(Task *thisTask) {
-	printf(sepa);
 	printf("| %d  + %d         + %s        + %d                | \n\r",
 			thisTask->ID, thisTask->priority, thisTask->nameTask,
-			thisTask->remaningExe);
+			thisTask->remainingExe);
+	printf("%s", sepa);
 }
 
 Task* newTaskElement(Task *actualTask, int idT) {
 	actualTask->ID = idT;
 	getTaskName(actualTask);
 	actualTask->priority = getPriority();
-	actualTask->remaningExe = getExeNumber();
+	actualTask->remainingExe = getExeNumber();
 	(*actualTask).nextTask = malloc(sizeof(Task));
 	return (*actualTask).nextTask;
 }
 
-void deleteTask(Task *actualTask) {
-	while (1) {
+void modifyPriority(Task *t) {
+	t = selectTask(t);
+	t->priority = getPriority();
+	return;
+}
 
-	}
+void modifyExecNumb(Task *t) {
+	t = selectTask(t);
+	t->remainingExe = getExeNumber();
+	return;
 }
 
 char switchPolicy(char pol) {
+	printf("\n\rYou switched the policy of scheduling from ");
 	if (pol == 'p') {
+		printf("PRIORITY ");
+		printf("to ");
+		printf("REMAINING EXECUTIONS\n\r");
 		return 'e';
 	} else if (pol == 'e') {
+		printf("REMAINING EXECUTIONS ");
+		printf("to ");
+		printf("PRIORITY \n\r");
 		return 'p';
 	}
 	return 'p';
