cc7db65d9f1c7026512f3ff13288f4742c21c1d4
Wabri <gabriele.puliti@gmail.com>
2017-05-31 11:37:13 +0200

    solve a problem with pointer in the definition of structure Task

--- progetto_so/procScheduler/scheduler.h
+++ progetto_so/procScheduler/scheduler.h
@@ -6,28 +6,31 @@
  */
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct TaskElement {
 	int ID;
 	char nameTask[8];
 	int priority; // number between 0 and 9
 	int remainingExe; // max 99 execution per task
-	struct Task *nextTask; // task successivo
+	struct TaskElement *nextTask;
 } Task;
 
-void newTaskElement(Task*, int, char[], int, int);
+Task* newTaskElement(Task*, int, char[], int, int);
 int getChoice();
+void printAllTasks(Task*);
 void printTask(Task*);
 int createID(Task*);
 char* getName(Task*);
 int getPriority(Task*);
-int getRemainingExecution(Task*);
+int getRemainingexecution(Task*);
 
 int runScheduling() {
+	int idTraker = 1;
 	int flag = 1;
 	Task *firstTask = malloc(sizeof(Task));
-	Task *actualTask;
-	actualTask = firstTask;
+	Task *lastTask; // the last Task is always empty
+	lastTask = firstTask;
 	printf("               This is a Scheduler Process\n\r");
 	printf(
 			".......................................................................\n\r");
@@ -37,10 +40,14 @@ int runScheduling() {
 			printf("Bye!!!\n\r");
 			return 0;
 		case 1:
-			printTask(firstTask);
+			printAllTasks(firstTask);
 			break;
 		case 2:
-			newTaskElement(actualTask, 10, "test", 4, 5);
+			printTask(firstTask->nextTask);
+			break;
+		case 3:
+			lastTask = newTaskElement(lastTask, idTraker, "test", 4, 5);
+			idTraker += 1;
 			break;
 		default:
 			flag = 0;
@@ -52,7 +59,8 @@ int runScheduling() {
 
 int getChoice() {
 	printf("You can chose to:\n\r");
-	printf(" 0) Exit\n\r 1) Print Tasks\n\r 2) Create new Task\n\r");
+	printf(
+			" 0) Exit\n\r 1) Print Task\n\r 2) Print all tasks\n\r 3) Create new Task\n\r");
 	int res = 0;
 	printf("> ");
 	scanf("%i", &res);
@@ -60,22 +68,34 @@ int getChoice() {
 	return res;
 }
 
-void printTask(Task *first) {
+void printAllTasks(Task *first) {
 	printf("The list of task is: \n\r");
-	for (Task *i = (Task*)first; i == NULL; i =(Task*) i->nextTask) {
-		printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n");
-		printf("| %d + %d         + %s        + %d                | ", i->ID,
-				i->priority, i->nameTask, i->priority);
-		printf("+----+-----------+-----------+-------------------+ \n");
+	printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n");
+	if (first->ID != 0) {
+		for (Task *i = first; i == NULL; i = i->nextTask) {
+			printf("+----+-----------+-----------+-------------------+ \n\r");
+			printf("| %d + %d         + %s        + %d                | \n\r",
+					i->ID, i->priority, i->nameTask, i->remainingExe);
+			printf("+----+-----------+-----------+-------------------+ \n\r");
+		}
 	}
 }
 
-void newTaskElement(Task *actualTask, int idT, char nameT[], int priorityT,
+void printTask(Task *thisTask) {
+	printf("This Task have the value of:\n\r");
+	printf("| ID + PRIORITA\' + NOME TASK + ESECUZ. RIMANENTI | \n\r");
+	printf("| %d  + %d         + %s          + %d                 | \n\r",
+			thisTask->ID, thisTask->priority, thisTask->nameTask,
+			thisTask->remainingExe);
+	printf("+----+-----------+-----------+-------------------+ \n\r");
+}
+
+Task* newTaskElement(Task *actualTask, int idT, char nameT[], int priorityT,
 		int execT) {
-	actualTask = malloc(sizeof(Task));
 	actualTask->ID = idT;
 	strcpy(actualTask->nameTask, nameT);
 	actualTask->priority = priorityT;
 	actualTask->remainingExe = execT;
-	actualTask->nextTask = NULL;
+	actualTask->nextTask = malloc(sizeof(Task));
+	return actualTask->nextTask;
 }
