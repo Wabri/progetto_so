2e2f675651cca1f94d9db10c974496118864c570
pagano <pagano@debian>
2017-06-23 17:33:08 +0200

    prima versione relazione

---
+++ progetto_so/relazione/exe/cmd.h
@@ -0,0 +1,73 @@
+#include <stdio.h>    
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_CMD_LEN 100
+
+// init folder ../commandexe/data/[pid] to store the logs
+int initDataFolder() {
+	char cmd[30];
+	FILE *fp;
+	sprintf(cmd, "%s%i", "mkdir -p ../commandexe/data/", getpid());
+	fp = popen(cmd, "r");
+	if (fp == NULL) {
+		printf("[Error] - Error initialing process folder\n");
+		exit(1);
+	}
+	return 0;
+}
+
+// function that execute the c command and log the output in ../commandexe/data/[pid]/out.[index]
+int execCommandAndLog(char* c, int index) {
+	FILE *fp;
+	char path[1035];
+	char filename[7];
+
+	sprintf(filename, "../commandexe/data/%i/%s.%i", getpid(), "out", index);
+	FILE *f = fopen(filename, "w");
+	if (f == NULL) {
+		printf("[Error] - Error opening file!\n");
+		exit(1);
+	}
+
+	// command open to read
+	sprintf(c, "%s %s", c, "2>&1");
+	fp = popen(c, "r");
+
+	if (fp == NULL) {
+		fprintf(f, "[Error] - Error executing the command\n");
+	}
+
+	// read the output a line at a time - output it.
+	while (fgets(path, sizeof(path) - 1, fp) != NULL) {
+		fprintf(f, "%s", path);
+	}
+
+	// closing files
+	pclose(fp);
+	fclose(f);
+
+	return 0;
+}
+
+int cmd_out() {
+	int k = 1;
+	initDataFolder();
+	while (1) {
+		char cmd[MAX_CMD_LEN] = "";
+		printf("\nEnter the %d-cmd: ", k);
+		//read chars until \n
+		scanf("%[^\n]", cmd);
+		getchar();
+		printf("Cmd entered : %s\n", cmd);
+		if (strlen(cmd) == 0) {
+			printf("Bye!\n");
+			exit(1);
+		}
+		execCommandAndLog(cmd, k);
+		k = k + 1;
+	}
+
+	return (0);
+}
---
+++ progetto_so/relazione/sched/scheduler.c
@@ -0,0 +1,190 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: 25 mag 2017
+ *      Authors: wabri, pagano
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "taskmanager.h"
+
+int getChoice(void);
+char switchPolicy(char pol);
+Task* sortListByPriority(Task*);
+Task* sortListByExecution(Task*);
+Task* swapTask(Task*, Task*, Task*);
+
+int main() {
+	int idTraker = 1;
+	int flag = 1;
+	char policy = 'p';
+	Task *firstTask = malloc(sizeof(Task));
+	Task *lastTask = NULL; // the last Task is always empty
+	Task *tmpTask;
+	printf(POINTSHEAD);
+	printf("               This is a process scheduler\n\r");
+	printf(POINTSHEAD);
+	while (flag == 1) {
+		switch (getChoice()) {
+		case 0:
+			printf("Bye!\n\r");
+			return 0;
+		case 1:
+			if (firstTask->ID == 0) {
+				lastTask = newTaskElement(firstTask, idTraker);
+			} else {
+				lastTask = newTaskElement(lastTask, idTraker);
+				if (policy == 'p') {
+					firstTask = sortListByPriority(firstTask);
+				} else if (policy == 'e') {
+					firstTask = sortListByExecution(firstTask);
+				}
+			}
+			idTraker += 1;
+			break;
+		case 2:
+			printf("\n\rHow many execution do you want to do: ");
+			scanf("%d", &flag);
+			while (flag != 0) {
+				if (executeTask(firstTask) == 0) {
+					firstTask = deleteTask(firstTask, firstTask);
+				}
+				flag -= 1;
+			}
+			flag = 1;
+			printf("\n\r");
+			break;
+		case 3:
+			tmpTask = selectTask(firstTask);
+			if (executeTask(tmpTask) == 0) {
+				firstTask = deleteTask(firstTask, tmpTask);
+			}
+			break;
+		case 4:
+			firstTask = deleteTask(firstTask, selectTask(firstTask));
+			break;
+		case 5:
+			modifyPriority(firstTask);
+			if (policy == 'p') {
+				firstTask = sortListByPriority(firstTask);
+			}
+			break;
+		case 6:
+			policy = switchPolicy(policy);
+			if (policy == 'p') {
+				firstTask = sortListByPriority(firstTask);
+			} else if (policy == 'e') {
+				firstTask = sortListByExecution(firstTask);
+			}
+			break;
+		case 7:
+			modifyExecNumb(firstTask);
+			if (policy == 'e') {
+				firstTask = sortListByExecution(firstTask);
+			}
+			break;
+		default:
+			flag = 0;
+			break;
+		}
+		if (!isEmptyTaskList(firstTask)) {
+			printf("\n\rScheduling Policy: ");
+			if (policy == 'p') {
+				printf("PRIORITY \n\r");
+			} else if (policy == 'e') {
+				printf("REMAINING EXECUTIONS \n\r");
+			}
+			printListTasks(firstTask);
+		}
+	}
+	return 0;
+}
+
+int getChoice() {
+	printf("\n\rPlease select an option:\n\r");
+	printf(" 0) Exit\n\r 1) Create a new task\n\r 2) Execute the task on the top of the list\n\r");
+	printf(
+			" 3) Execute a task\n\r 4) Delete a task\n\r 5) Modify the PRIORITY of a task\n\r");
+	printf(
+			" 6) Switch policy (default : PRIORITY)\n\r 7) Modify the REMAINING EXECUTIONS of a task");
+	int res = 0;
+	printf("\n\r> ");
+	scanf("%i", &res);
+	return res;
+}
+
+char switchPolicy(char pol) {
+	printf("\n\rYou switched the policy of scheduling from ");
+	if (pol == 'p') {
+		printf("PRIORITY to REMAINING EXECUTIONS\n\r");
+		return 'e';
+	} else if (pol == 'e') {
+		printf("REMAINING EXECUTIONS to PRIORITY \n\r");
+		return 'p';
+	}
+	return 'p';
+}
+
+Task* sortListByPriority(Task *headTask) {
+	Task *tempTask = headTask;
+	Task *previousTempTask = tempTask;
+	int flag = 0;
+	while (!flag) {
+		flag = 1;
+		tempTask = headTask;
+		previousTempTask = tempTask;
+		while (tempTask->ID != 0) {
+			if (tempTask->priority < tempTask->nextTask->priority) {
+				if (tempTask == headTask) {
+					headTask = swapTask(headTask, tempTask, tempTask->nextTask);
+				} else {
+					previousTempTask = swapTask(previousTempTask, tempTask,
+							tempTask->nextTask);
+				}
+				flag = 0;
+			}
+			previousTempTask = tempTask;
+			tempTask = tempTask->nextTask;
+		}
+	}
+	return headTask;
+}
+
+Task* sortListByExecution(Task* headTask) {
+	Task *tempTask = headTask;
+	Task *previousTempTask = tempTask;
+	int flag = 0;
+	while (!flag) {
+		flag = 1;
+		tempTask = headTask;
+		previousTempTask = tempTask;
+		while (tempTask->ID != 0) {
+			if ((tempTask->remainingExe > tempTask->nextTask->remainingExe)
+					&& (tempTask->nextTask->remainingExe != 0)) {
+				if (tempTask == headTask) {
+					headTask = swapTask(headTask, headTask, headTask->nextTask);
+				} else {
+					previousTempTask = swapTask(previousTempTask, tempTask,
+							tempTask->nextTask);
+				}
+				flag = 0;
+			}
+			previousTempTask = tempTask;
+			tempTask = tempTask->nextTask;
+		}
+	}
+	return headTask;
+}
+
+Task* swapTask(Task *previousTask, Task *taskSwap1, Task *taskSwap2) {
+	if (previousTask != taskSwap1) {
+		previousTask->nextTask = taskSwap2;
+		taskSwap1->nextTask = taskSwap2->nextTask;
+		taskSwap2->nextTask = taskSwap1;
+		return previousTask;
+	}
+	taskSwap1->nextTask = taskSwap2->nextTask;
+	taskSwap2->nextTask = taskSwap1;
+	return taskSwap2;
+}
