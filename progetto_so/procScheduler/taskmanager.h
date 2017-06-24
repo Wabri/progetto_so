23c26a2f6283f3a50c0da4535be7efe84bb7fd0e
Wabri <gabriele.puliti@gmail.com>
2017-06-24 17:13:19 +0200

    create comment for all the function in the taskManager.h

--- progetto_so/procScheduler/taskmanager.h
+++ progetto_so/procScheduler/taskmanager.h
@@ -25,6 +25,13 @@ void printListTasks(Task*);
 Task* deleteTask(Task*, Task*);
 int executeTask(Task*);
 
+/*
+*
+* PURPOSE : Setter remainingExe member of struct TaskElement, this function is use by newTaskElement
+*
+* RETURN : int -> number of remaining execution
+*
+*/
 int setExeNumber() {
 	int exeNum = 0;
 	do {
@@ -37,6 +44,13 @@ int setExeNumber() {
 	return exeNum;
 }
 
+/*
+*
+* PURPOSE : Setter priority member of struct TaskElement, this function is use by newTaskElement
+*
+* RETURN : int -> number of priority 
+*
+*/
 int setPriority() {
 	int priority = 0;
 	do {
@@ -49,6 +63,13 @@ int setPriority() {
 	return priority;
 }
 
+/*
+*
+* PURPOSE : Setter name member of struct TaskElement, this function is use by newTaskElement
+* PARAMS : Task* actualTask -> pointer of the task that want to set name
+* RETURN : void
+*
+*/
 void setTaskName(Task *actualTask) {
 	char name[256];
 	do {
@@ -69,27 +90,45 @@ void setTaskName(Task *actualTask) {
 	return;
 }
 
-int isEmptyTaskList(Task *actualTask) {
-	if (!(actualTask->ID)) {
-		return 1;
-	}
-	return 0;
+/*
+*
+* PURPOSE : Check if the list is empty
+* PARAMS : Task* firstTask -> pointer of the first (head) task of the list
+* RETURN : int -> return 0 if the list is empty, return 1 if the list is not empty
+*
+*/
+int isEmptyTaskList(Task *firstTask) {
+	return !(firstTask->ID);
 }
 
-Task* selectTask(Task* actualTask) {
+/*
+*
+* PURPOSE : It ask the ID of the Task and finds the Task with that ID
+* PARAMS : Task* firstTask -> pointer of the first (head) task of the list
+* RETURN : Task* -> return the pointer of the task found
+*
+*/
+Task* selectTask(Task* firstTask) {
 	int id;
 	printf("Select the task...\nInsert the ID : ");
 	scanf("%d", &id);
-	while (actualTask->ID != id) {
-		actualTask = actualTask->nextTask;
-		if (actualTask == NULL) {
+	while (firstTask->ID != id) {
+		firstTask = firstTask->nextTask;
+		if (firstTask == NULL) {
 			printf("\n\rError! No tasks with this ID!\n\r");
-			return actualTask;
+			return firstTask;
 		}
 	}
-	return actualTask;
+	return firstTask;
 }
 
+/*
+*
+* PURPOSE : Modify the priority of the task
+* PARAMS : Task* thisTask -> pointer of the task to change
+* RETURN : void
+*
+*/
 void modifyPriority(Task *thisTask) {
 	thisTask = selectTask(thisTask);
 	if (thisTask == NULL) {
@@ -99,6 +138,13 @@ void modifyPriority(Task *thisTask) {
 	return;
 }
 
+/*
+*
+* PURPOSE : Modify the execution number of the task
+* PARAMS : Task* thisTask -> pointer of the task to change
+* RETURN : void
+*
+*/
 void modifyExecNumb(Task *thisTask) {
 	thisTask = selectTask(thisTask);
 	if (thisTask == NULL) {
@@ -108,6 +154,14 @@ void modifyExecNumb(Task *thisTask) {
 	return;
 }
 
+/*
+*
+* PURPOSE : Allocate a new item in the list
+* PARAMS : Task* actualTask -> pointer of the last task of the list
+* PARAMS : int idT -> the id of the new task
+* RETURN : Task* -> pointer of the new last task of the list
+*
+*/
 Task* newTaskElement(Task *actualTask, int idT) {
 	actualTask->ID = idT;
 	setTaskName(actualTask);
@@ -117,6 +171,13 @@ Task* newTaskElement(Task *actualTask, int idT) {
 	return (*actualTask).nextTask;
 }
 
+/*
+*
+* PURPOSE : Print a single Task
+* PARAMS : Task* thisTask -> pointer of the task to print
+* RETURN : void
+*
+*/
 void printTask(Task *thisTask) {
 	printf("| %d  + %d         + %s        + %d                | \n\r",
 			thisTask->ID, thisTask->priority, thisTask->nameTask,
@@ -124,6 +185,13 @@ void printTask(Task *thisTask) {
 	printf(SEPARATOR);
 }
 
+/*
+*
+* PURPOSE : Print the list of the task
+* PARAMS : Task* first -> printing start from this task
+* RETURN : void
+*
+*/
 void printListTasks(Task *first) {
 	printf(SEPARATOR);
 	printf("| ID + PRIORITY + TASK NAME + REMAINING EXEC |\n\r");
@@ -135,6 +203,14 @@ void printListTasks(Task *first) {
 	}
 }
 
+/*
+*
+* PURPOSE : Delete a Task
+* PARAMS : Task* first -> pointer of the first task of the list
+* PARAMS : Task* thisTask -> pointer of the task to delete
+* RETURN : Task* -> return the pointer of the first task of the list
+*
+*/
 Task* deleteTask(Task *first, Task *thisTask) {
 	if (thisTask != NULL) {
 		Task *tmpTask = first;
@@ -162,6 +238,13 @@ Task* deleteTask(Task *first, Task *thisTask) {
 	return first;
 }
 
+/*
+*
+* PURPOSE : Execute a Task
+* PARAMS : Task* thisTask -> pointer of the task to execute
+* RETURN : Task* -> return the number of the remaining execution
+*
+*/
 int executeTask(Task *thisTask) {
 	if (thisTask != NULL) {
 		thisTask->remainingExe -= 1;
